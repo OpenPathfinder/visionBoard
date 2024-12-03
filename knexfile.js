ac8ad1aed87d258b4a7505c4716424fe68bbfd67
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-03 21:23:36 +0100

    chore: migrate knex configuration to config handler

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,6 @@
-const { normalizeBoolean } = require('@ulisesgascon/normalize-boolean')
-const dbSettings = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST || '0.0.0.0',
-    user: process.env.DB_USER || 'openjs',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'dashboard'
-  },
-  ssl: normalizeBoolean(process.env.DB_SSL),
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: './src/database/migrations'
-  },
-  seeds: {
-    directory: './src/database/seeds'
-  }
-}
+const { getConfig } = require('./src/config')
+
+const { dbSettings } = getConfig()
 
 module.exports = {
   development: dbSettings
--- src/config/index.js
+++ src/config/index.js
@@ -1,19 +1,43 @@
+const { normalizeBoolean } = require('@ulisesgascon/normalize-boolean')
 const projectCategories = ['impact', 'at-large', 'incubation', 'emeritus']
 
+const dbSettings = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST || '0.0.0.0',
+    user: process.env.DB_USER || 'openjs',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'dashboard'
+  },
+  ssl: normalizeBoolean(process.env.DB_SSL),
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: './src/database/migrations'
+  },
+  seeds: {
+    directory: './src/database/seeds'
+  }
+}
+
 const defaultValues = {
-    projectCategories
+  projectCategories,
+  dbSettings
 }
 const testEnvironment = {
-    projectCategories
+  projectCategories,
+  dbSettings
 }
 
 const getConfig = env => {
-    // NOTE: env variable should override the NODE_ENV
-    const environment = env || process.env.NODE_ENV
-    const config = environment === 'test' ? testEnvironment : defaultValues
-    return config
+  // NOTE: env variable should override the NODE_ENV
+  const environment = env || process.env.NODE_ENV
+  const config = environment === 'test' ? testEnvironment : defaultValues
+  return config
 }
-  
+
 module.exports = {
-    getConfig
+  getConfig
 }

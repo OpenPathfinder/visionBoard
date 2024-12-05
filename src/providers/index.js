24f477e1f21962cd340ba94441676dec0aaff0de
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-06 00:33:10 +0100

    feat: add method `fetchOrgReposListByLogin` in GitHub provider

--- src/providers/index.js
+++ src/providers/index.js
@@ -12,8 +12,39 @@ const fetchOrgByLogin = async (login) => {
   return data
 }
 
+const fetchOrgReposListByLogin = async (login) => {
+  debug(`Fetching organization (${login}) repositories...`)
+  ensureGithubToken()
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+  let repoList = []
+
+  // IMPORTANT: Ignore private repositories as they might contain sensitive information
+  const orgQuery = { org: login, type: 'public', per_page: 100 }
+
+  const { data: repos } = await octokit.rest.repos.listForOrg(orgQuery)
+  debug(`Got ${repos.length} repos for org: ${login}`)
+  repoList = repoList.concat(repos)
+
+  // IMPORTANT: If the org has 100 repos it might require pagination management
+  if (repoList.length === 100) {
+    let page = 2
+    let hasMore = true
+    while (hasMore) {
+      debug(`Getting page ${page} for org: ${login}`)
+      const { data: repos, headers } = await octokit.rest.repos.listForOrg({ ...orgQuery, page })
+      debug(`Got ${repos.length} repos for org: ${login}`)
+      repoList = repoList.concat(repos)
+      hasMore = headers.link.includes('rel="next"')
+      page += 1
+    }
+  }
+
+  return repoList
+}
+
 const github = {
-  fetchOrgByLogin
+  fetchOrgByLogin,
+  fetchOrgReposListByLogin,
 }
 
 module.exports = {

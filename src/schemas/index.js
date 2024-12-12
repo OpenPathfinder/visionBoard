42e905db671401999700985b38707b9306a0ce01
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-12 19:48:19 +0100

    feat: add JSON Schema validations for the OpenSSF Scorecard results

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,6 +3,7 @@ const addFormats = require('ajv-formats')
 const githubOrganizationSchema = require('./githubOrganization.json')
 const githubListOrgReposSchema = require('./githubListOrgRepos.json')
 const githubRepositorySchema = require('./githubRepository.json')
+const ossfScorecardResultSchema = require('./ossfScorecardResult.json')
 
 const ajv = new Ajv()
 addFormats(ajv)
@@ -39,8 +40,19 @@ const validateGithubRepository = (data) => {
   return null
 }
 
+const validateOSSFResult = (data) => {
+  const validate = ajv.compile(ossfScorecardResultSchema)
+  const valid = validate(data)
+  if (!valid) {
+    const readableErrors = getReadableErrors(validate)
+    throw new Error(`Error when validating the OSSF Scorecard result: ${readableErrors}`)
+  }
+  return null
+}
+
 module.exports = {
   validateGithubOrg,
   validateGithubListOrgRepos,
-  validateGithubRepository
+  validateGithubRepository,
+  validateOSSFResult
 }

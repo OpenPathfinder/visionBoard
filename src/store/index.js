4ad876ab846cfbad12c1f897761063c9f43f2d60
Sebastian Beltran <bjohansebas@gmail.com>
2025-01-09 14:26:09 -0500

    create necessary functions for the OWASP table

--- src/store/index.js
+++ src/store/index.js
@@ -125,6 +125,13 @@ const getAllSSoftwareDesignTrainingsByProjectIds = (knex, projectIds) => {
     .select('*')
 }
 
+const getAllOwaspTrainingsByProjectIds = (knex, projectIds) => {
+  debug(`Fetching all owasp trainings by project ids (${projectIds})...`)
+  return knex('owasp_training')
+    .whereIn('owasp_training.project_id', projectIds)
+    .select('*')
+}
+
 const initializeStore = (knex) => {
   debug('Initializing store...')
   const getAll = getAllFn(knex)
@@ -138,6 +145,7 @@ const initializeStore = (knex) => {
     getAllComplianceChecks: () => getAll('compliance_checks'),
     getAllProjects: () => getAll('projects'),
     getAllSSoftwareDesignTrainings: () => getAll('software_design_training'),
+    getAllOwaspTrainings: () => getAll('owasp_training'),
     getAllGithubRepositories: () => getAll('github_repositories'),
     getAllChecklists: () => getAll('compliance_checklists'),
     getAllResults: () => getAll('compliance_checks_results'),
@@ -146,6 +154,7 @@ const initializeStore = (knex) => {
     getAllChecksInChecklistById,
     getAllGithubOrganizationsByProjectsId: (projectIds) => getAllGithubOrganizationsByProjectsId(knex, projectIds),
     getAllSSoftwareDesignTrainingsByProjectIds: (projectIds) => getAllSSoftwareDesignTrainingsByProjectIds(knex, projectIds),
+    getAllOwaspTrainingsByProjectIds: (projectIds) => getAllOwaspTrainingsByProjectIds(knex, projectIds),
     getCheckByCodeName: getCheckByCodeName(knex),
     deleteTasksByComplianceCheckId: deleteTasksByComplianceCheckId(knex),
     deleteAlertsByComplianceCheckId: deleteAlertsByComplianceCheckId(knex),
@@ -153,6 +162,7 @@ const initializeStore = (knex) => {
     addTask: (task) => addTo('compliance_checks_tasks', task),
     addResult: (result) => addTo('compliance_checks_results', result),
     addSSoftwareDesignTraining: (data) => addTo('software_design_training', data),
+    addOwaspTraining: (data) => addTo('owasp_training', data),
     addGithubRepo: (repo) => addTo('github_repositories', repo),
     addOSSFScorecardResult: (ossf) => addTo('ossf_scorecard_results', ossf),
     upsertOSSFScorecard: upsertOSSFScorecard(knex),

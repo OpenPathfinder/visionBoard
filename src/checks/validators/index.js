727a8082c207595607df8b0e23efc5e47c8009c0
Sebastian Beltran <bjohansebas@gmail.com>
2025-01-09 14:45:17 -0500

    create test for owasp training check validator

--- src/checks/validators/index.js
+++ src/checks/validators/index.js
@@ -1,10 +1,12 @@
 const githubOrgMFA = require('./githubOrgMFA')
 const softwareDesignTraining = require('./softwareDesignTraining')
+const owaspTraining = require('./owaspTraining')
 const adminRepoCreationOnly = require('./adminRepoCreationOnly')
 
 const validators = {
   githubOrgMFA,
   softwareDesignTraining,
+  owaspTraining,
   adminRepoCreationOnly
 }
 

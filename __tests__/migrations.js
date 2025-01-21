b67b6f149248880c288c16f18ac6d6f3bed5f4a5
Guillermo C. Martínez <telekosmos@mymail.com>
2025-01-21 19:55:36 +0100

    chore: tests for the migration

---
+++ __tests__/migrations.js
@@ -0,0 +1,50 @@
+const knexInit = require('knex')
+const { getConfig } = require('../src/config')
+
+const { dbSettings } = getConfig('test')
+
+const {
+  resetDatabase
+} = require('../__utils__')
+
+let knex
+
+describe('Migrations', () => {
+  beforeAll(async () => {
+    knex = knexInit(dbSettings)
+    await knex.migrate.latest()
+  })
+  beforeEach(async () => {
+    await resetDatabase(knex)
+  })
+  afterAll(async () => {
+    await knex.destroy()
+  })
+
+  it('should run migrations successfully', async () => {
+    const result = await knex.migrate.latest()
+    expect(result).not.toBeNull()
+    const complianceChecksCount = await knex('compliance_checks').count()
+    expect(complianceChecksCount.length).toBe(1)
+  })
+
+  it('should restore compliance_checks table after rolling back', async () => {
+    const result = await knex.migrate.latest()
+    expect(result).not.toBeNull()
+    const complianceChecksCountBeforeRollback = await knex('compliance_checks')
+      .select('*')
+      .limit(1)
+    expect(complianceChecksCountBeforeRollback.length).toBe(1)
+    expect(complianceChecksCountBeforeRollback[0]).not.toHaveProperty('mitre_url')
+    expect(complianceChecksCountBeforeRollback[0]).not.toHaveProperty('mitre_description')
+
+    await knex.migrate.rollback()
+
+    const complianceChecksCountAfterRollback = await knex('compliance_checks')
+      .select('*')
+    complianceChecksCountAfterRollback.forEach((item) => {
+      expect(item).toHaveProperty('mitre_url')
+      expect(item).toHaveProperty('mitre_description')
+    })
+  })
+})

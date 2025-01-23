7fee001d83d661c70b369c16d14064f93b195e96
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2025-01-23 11:46:35 +0100

    test: revert tests for check resources migration

--- __utils__/index.js
+++ __utils__/index.js
@@ -5,8 +5,6 @@ const resetDatabase = async (knex) => {
   await knex.raw('TRUNCATE TABLE compliance_checks_results RESTART IDENTITY CASCADE')
   await knex.raw('TRUNCATE TABLE compliance_checks_tasks RESTART IDENTITY CASCADE')
   await knex.raw('TRUNCATE TABLE compliance_checks_alerts RESTART IDENTITY CASCADE')
-  await knex.raw('TRUNCATE TABLE compliance_checks_resources RESTART IDENTITY CASCADE')
-  await knex.raw('TRUNCATE TABLE resources_for_compliance_checks RESTART IDENTITY CASCADE')
   await knex.raw('TRUNCATE TABLE github_repositories RESTART IDENTITY CASCADE')
   await knex.raw('TRUNCATE TABLE github_organizations RESTART IDENTITY CASCADE')
   await knex.raw('TRUNCATE TABLE projects RESTART IDENTITY CASCADE')

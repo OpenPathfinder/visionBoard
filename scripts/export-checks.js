a0705bb1adc87cb98747872828f947c3916cd344
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2025-01-23 14:11:31 +0100

    feat: add support for multiple resources when exporting the checks

--- scripts/export-checks.js
+++ scripts/export-checks.js
@@ -5,8 +5,36 @@ const knex = require('knex')(dbSettings)
 const { join } = require('path')
 
 ;(async () => {
-  const checks = await knex('compliance_checks').select()
-  writeFileSync(join(process.cwd(), 'output', 'checks.json'), JSON.stringify(checks, null, 2))
+  const checks = await knex('compliance_checks')
+    .leftJoin('resources_for_compliance_checks', 'compliance_checks.id', 'resources_for_compliance_checks.compliance_check_id')
+    .leftJoin('compliance_checks_resources', 'resources_for_compliance_checks.compliance_check_resource_id', 'compliance_checks_resources.id')
+    .select(
+      'compliance_checks.*',
+      knex.raw(`
+        json_agg(
+          json_build_object(
+            'type', compliance_checks_resources.type,
+            'url', compliance_checks_resources.url,
+            'name', compliance_checks_resources.name
+          )
+        ) as resources
+      `)
+    )
+    .groupBy('compliance_checks.id')
+
+  const formattedChecks = checks.map(check => {
+    const resources = check.resources.reduce((acc, resource) => {
+      if (!acc[resource.type]) {
+        acc[resource.type] = []
+      }
+      acc[resource.type].push({ url: resource.url, name: resource.name })
+      return acc
+    }, { mitre: [], how_to: [], sources: [] })
+
+    return { ...check, resources }
+  })
+
+  writeFileSync(join(process.cwd(), 'output', 'checks.json'), JSON.stringify(formattedChecks, null, 2))
   console.log('Checks exported to checks.json')
   await knex.destroy()
 })()

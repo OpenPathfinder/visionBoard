35ee87bfee8719b892232e0ea191104d670f2ff3
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2025-06-15 16:55:36 +0200

    feat: add `POST /api/v1/project` endpoint (#239)

--- src/httpServer/routers/apiV1.js
+++ src/httpServer/routers/apiV1.js
@@ -1,12 +1,45 @@
 const { generateStaticReports } = require('../../reports')
 const pkg = require('../../../package.json')
 const { logger } = require('../../utils')
+const { initializeStore } = require('../../store')
+const _ = require('lodash')
+const { isSlug } = require('validator')
 
 function createApiRouter (knex, express) {
+  const { addProject, getProjectByName } = initializeStore(knex)
+
   const router = express.Router()
 
   router.get('/__health', (req, res) => {
     res.json({ status: 'ok', timestamp: new Date().toISOString(), version: pkg.version, name: pkg.name })
+  })
+
+  router.post('/project', async (req, res) => {
+    try {
+      // Validate request body
+      const { name } = req.body
+      const projectName = _.kebabCase(name)
+
+      // Check data and database
+      if (!projectName || !isSlug(projectName)) {
+        return res.status(400).json({ errors: [{ message: 'Invalid project name. Must be a slug.' }] })
+      }
+      const existingProject = await getProjectByName(projectName)
+      if (existingProject) {
+        return res.status(409).json({ errors: [{ message: 'Project already exists.' }] })
+      }
+
+      // Modify database
+      const project = await addProject({ name: projectName })
+
+      // Return response
+      return res.status(201)
+        .header('Location', `/api/v1/project/${project.id}`)
+        .json(project)
+    } catch (err) {
+      logger.error(err)
+      return res.status(500).json({ errors: [{ message: 'Internal server error' }] })
+    }
   })
 
   router.post('/generate-reports', async (req, res) => {

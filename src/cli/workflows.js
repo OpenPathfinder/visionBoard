ef0b1bb61d80caac4739202ae15b777421c1ba06
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2025-06-25 12:38:53 +0200

    Merge pull request #254 from OpenPathfinder/feat/remove-ops

--- src/cli/workflows.js
+++ src/cli/workflows.js
@@ -3,37 +3,32 @@ const _ = require('lodash')
 const debug = require('debug')('cli:workflows')
 const { updateGithubOrgs, upsertGithubRepositories, runAllTheComplianceChecks, runOneComplianceCheck, upsertOSSFScorecardAnalysis } = require('../workflows')
 const { generateStaticReports } = require('../reports')
-const { bulkImport } = require('../importers')
 const { logger } = require('../utils')
-const { executeOneCheckSchema, executeOptionalProjectSchema, bulkImportSchema } = require('../schemas')
+const { executeOneCheckSchema, executeOptionalProjectSchema } = require('../schemas')
 
 const commandList = [{
   name: 'update-github-orgs',
   isRequiredAdditionalData: false,
   isEnabled: true,
   description: 'Check the organizations stored and update the information with the GitHub API.',
-  operations: null,
   workflow: updateGithubOrgs
 }, {
   name: 'upsert-github-repositories',
   isRequiredAdditionalData: false,
   isEnabled: true,
   description: 'Check the organizations stored and update/create the information related to the repositories with the GitHub API.',
-  operations: null,
   workflow: upsertGithubRepositories
 }, {
   name: 'run-all-checks',
   isRequiredAdditionalData: false,
   isEnabled: true,
   description: 'Run all the compliance checks for the stored data.',
-  operations: null,
   workflow: runAllTheComplianceChecks
 }, {
   name: 'run-one-check',
   isRequiredAdditionalData: true,
   isEnabled: true,
   description: 'Run a specific compliance check for the stored data.',
-  operations: null,
   schema: executeOneCheckSchema,
   workflow: runOneComplianceCheck
 }, {
@@ -42,27 +37,13 @@ const commandList = [{
   schema: executeOptionalProjectSchema,
   isEnabled: false,
   description: 'Upsert the OSSF Scorecard scoring by running and checking every repository in the database.',
-  operations: null,
   workflow: upsertOSSFScorecardAnalysis
 }, {
   name: 'generate-reports',
   isRequiredAdditionalData: false,
   isEnabled: true,
   description: 'Generate the reports for the stored data.',
-  operations: null,
   workflow: generateStaticReports
-}, {
-  // @TODO: Move this workflow to a separate endpoint
-  name: 'bulk-import',
-  isRequiredAdditionalData: true,
-  isEnabled: false,
-  operations: [{
-    id: 'load-manual-checks',
-    description: 'Load manual checks from project policies',
-    schema: JSON.stringify(bulkImportSchema)
-  }],
-  description: 'Bulk import data into visionBoard',
-  workflow: bulkImport
 }]
 
 const validCommandNames = commandList.map(({ name }) => name)
@@ -73,13 +54,12 @@ const getWorkflowsDetails = () => {
 
   commandList.forEach((workflow) => {
     const workflowName = _.kebabCase(workflow.name)
-    workflowsList.push({ id: workflowName, description: workflow.description, isEnabled: workflow.isEnabled, isRequiredAdditionalData: workflow.isRequiredAdditionalData, operations: workflow.operations, schema: JSON.stringify(workflow.schema) })
+    workflowsList.push({ id: workflowName, description: workflow.description, isEnabled: workflow.isEnabled, isRequiredAdditionalData: workflow.isRequiredAdditionalData, schema: JSON.stringify(workflow.schema) })
     workflows[workflowName] = {
       description: workflow.description,
       workflow: workflow.workflow,
       isEnabled: workflow.isEnabled,
       isRequiredAdditionalData: workflow.isRequiredAdditionalData,
-      operations: workflow.operations,
       schema: JSON.stringify(workflow.schema)
     }
   })
